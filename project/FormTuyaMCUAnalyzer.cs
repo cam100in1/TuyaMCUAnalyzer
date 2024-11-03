c7f8437285ead04f35f3b26cf37c780bf855b41a
theosoft <theosoft@hotmail.com>
2024-11-03 13:44:07 +0100

    Final push for pull req.

--- project/FormTuyaMCUAnalyzer.cs
+++ project/FormTuyaMCUAnalyzer.cs
@@ -24,8 +24,8 @@ namespace TuyaMCUAnalyzer
     {
         private bool refreshingComparer;
         private bool bUseVarsForVer0Cmd6InsteadOfDate = true;
-        private byte special_marker_recv = (byte)'R';
-        private byte special_marker_sent = (byte)'S';
+        private byte special_marker_recv = (byte)'r';
+        private byte special_marker_sent = (byte)'s';
         private int specialMarkerCount = 10;
         private string[] allPorts;
         private enum cellNames
